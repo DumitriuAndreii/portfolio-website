9f19b4c6428c5577d06d65330557d9ec74912270
DumitriuAndreii <andrei24ian@gmail.com>
2024-02-28 00:43:07 +0200

    change pdf

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ export const HeroSection = () => {
               Hire Me
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3">
-              <a href="cv.pdf" download="DumitriuAndrei_CV">
+              <a href="CV_DumitriuAndrei.pdf" download="DumitriuAndrei_CV">
                 <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                   Download CV
                 </span>
