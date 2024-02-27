c904357e07e59a55a63ce947f7659aadd1e490ec
DumitriuAndreii <andrei24ian@gmail.com>
2024-02-27 20:48:02 +0200

    projects section

---
+++ src/app/components/ProjectCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+export const ProjectCard = ({ image, title, description, gitURL, previewURL }) => {
+  return (
+    <div>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{ background: `url(${image})`, backgroundSize: "cover" }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+            <Link href={gitURL} className="h-14 w-14 mr-3 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/j">
+                <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover/j:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"  />
+            </Link>
+            <Link href={previewURL} className="h-14 w-14 mr-3 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/j">
+                <EyeIcon className="h-10 w-10 text-[#ADB7BE] cursor-pointer group-hover/j:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"  />
+            </Link>
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
+        <h5 className="font-xl font-semibold mb-2 ">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/components/ProjectTag.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+export const ProjectTag = ({ name, onClick, isSelected }) => {
+  const buttonStyle = isSelected
+    ? "text-white border-purple-500"
+    : "text-[#ADB7BE] border-slate-600 hover:border-white";
+  return (
+    <button
+      className={`${buttonStyle} rounded-full border-2 px-6 py-3 text-xl cursor-pointer `}
+      onClick={() => onClick(name)}
+    >
+      {name}
+    </button>
+  );
+};
