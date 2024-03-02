a5efdbe7fce79133530437ebce7cb608ef7a0bee
DumitriuAndreii <andrei24ian@gmail.com>
2024-03-02 03:17:52 +0200

    add contact me button and functionality

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,8 +35,16 @@ export const HeroSection = () => {
             professional endeavors.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white mt-3">
-              Hire Me
+            <button
+              className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white mt-3"
+              onClick={() => {
+                const section = document.querySelector("#contact");
+                if (section) {
+                  section.scrollIntoView({ behavior: "smooth" });
+                }
+              }}
+            >
+              Contact Me
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3">
               <a href="CV_DumitriuAndrei.pdf" download="DumitriuAndrei_CV">
